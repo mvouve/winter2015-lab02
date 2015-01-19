e72e0edc98713031df7c701f04221cda1f0d16ee
Marc Vouve <mvouve@gmail.com>
2015-01-18 16:58:25 -0800

    Updated website to use mySQL

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -21,6 +21,31 @@ class Gallery extends Application
      */
     public function index()
     {
+        // get all the iumages from model
+        $pix = $this->images->all();
+        
+        // build an array of cells
+        foreach($pix as $picture)
+        {
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        }
+        
+        //prime table class
+        $this->load->library('table');
+        $params = array
+        (
+            'table_open' => '<table class="gallery">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'                
+        );
+        
+        $this->table->set_template($params);
+        
+        //generate the table
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
+        
+        
         //$this->load->view('welcome');
         $this->data['pagebody'] = 'gallery';
         $this->render();
