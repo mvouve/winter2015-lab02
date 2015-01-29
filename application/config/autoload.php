92fe9b548fedbd0b68e3bc106dea2ed273d12705
Marc Vouve <mvouve@gmail.com>
2015-01-28 21:19:34 -0800

    oops

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -96,7 +96,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'parser');
 
 
 /*
@@ -173,7 +173,7 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('images');
 
 
 
